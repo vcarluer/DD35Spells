60f86215131b5a8d3a70579cc85318e8a9c51d1b
vcarluer <vcarluer@gmail.com>
2011-08-17 23:10:13 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DD35Spells::Application.config.secret_token = '85a8c73e14b90ad5648e0f3dd2567cf244ddc1ed37e2cf1c5688067dda2d505f5e49de861a2d9013b2023a7542c45cb6a0c08f8c40a03085888c8b1a23fc58ab'
